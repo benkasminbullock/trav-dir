cbb79a447fd3149af3c92471011be15f2169f2f9
Ben Bullock <benkasminbullock@gmail.com>
2021-02-17 14:09:48 +0900

    Add core content of module

--- lib/Trav/Dir.pm
+++ lib/Trav/Dir.pm
@@ -5,9 +5,125 @@ use Carp;
 use utf8;
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw//;
+our @EXPORT_OK = qw/find_files/;
 our %EXPORT_TAGS = (
     all => \@EXPORT_OK,
 );
 our $VERSION = '0.01';
+
+sub new
+{
+    my ($class, %options) = @_;
+    my $o;
+    if ($options{verbose}) {
+	$o->{verbose} = $options{verbose};
+	delete $options{verbose};
+    }
+    if ($options{rejfile}) {
+	$o->{rejfile} = $options{rejfile};
+	delete $options{rejfile};
+    }
+    if ($options{no_trav}) {
+	$o->{no_trav} = $options{no_trav};
+	delete $options{no_trav};
+    }
+    $o->{minsize} = 0;
+    if ($options{minsize}) {
+	$o->{minsize} = $options{minsize};
+	delete $options{minsize};
+    }
+    $o->{maxsize} = 'inf';
+    if ($options{maxsize}) {
+	$o->{maxsize} = $options{maxsize};
+	delete $options{maxsize};
+    }
+    if ($options{only}) {
+	$o->{only} = $options{only};
+	delete $options{only};
+    }
+    if ($options{callback}) {
+	$o->{callback} = $options{callback};
+	delete $options{callback};
+    }
+    for my $k (keys %options) {
+	warn "Unknown option $k";
+	delete $options{$k};
+    }
+    bless $o, $class;
+}
+
+sub find_files
+{
+    my ($o, $dir, $f) = @_;
+    my $dh;
+    if (! opendir ($dh, $dir)) {
+	warn "opendir $dir failed: $!";
+	return;
+    }
+    my @files = readdir ($dh);
+    closedir ($dh);
+    for my $file (@files) {
+	if ($file eq '.' || $file eq '..') {
+	    next;
+	}
+	if ($o->{rejfile} && $file =~ $o->{rejfile}) {
+	    if ($o->{verbose}) {
+		print "Skipping $file\n";
+	    }
+	    next;
+	}
+	my $dfile = "$dir/$file";
+	if ($o->{verbose}) {
+	    print "$dir $file\n";
+	}
+	if (-d $dfile) {
+	    if (! $o->{no_trav} || $dfile !~ $o->{no_trav}) {
+		if (-l $dfile) {
+		    # Skip symbolic links
+		    if ($o->{verbose}) {
+			print "Skipping symbolic link '$dfile'.\n";
+		    }
+		    next;
+		}
+		find_files ($o, $dfile, $f);
+	    }
+	}
+	if (-l $dfile) {
+	    # Skip symbolic links
+	    if ($o->{verbose}) {
+		print "Skipping symbolic link '$dfile'.\n";
+	    }
+	    next;
+	}
+	my $size = -s $dfile;
+	if ($size > $o->{maxsize} || $size < $o->{minsize}) {
+	    if ($o->{verbose}) {
+		print "Skipping $file due to size $size > $o->{maxsize} or < $o->{minsize}\n";
+	    }
+	    next;
+	}
+	my $safe = $dfile;
+	$safe =~ s![^[:print:]]!XX!g;
+	print "$safe\n";
+
+	if ($o->{only}) {
+	    if ($file =~ $o->{only}) {
+		$o->save ($f, $dfile);
+	    }
+	}
+	else {
+	    $o->save ($f, $dfile);
+	}
+    }
+}
+
+sub save
+{
+    my ($o, $f, $dfile) = @_;
+    push @$f, $dfile;
+    if ($o->{callback}) {
+	&{$o->{callback}} ($o, $dfile);
+    }
+}
+
 1;
